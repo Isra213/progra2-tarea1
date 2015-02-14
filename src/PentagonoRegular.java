9842cb4167849d79d5e173e3f26b60c253d12889
Isra213 <israeljosue@unitec.edu>
2015-02-14 16:45:35 -0600

    Update PentagonoRegular.java

--- src/PentagonoRegular.java
+++ src/PentagonoRegular.java
@@ -1,33 +1,25 @@
-import java.util.Scanner;
+// bueno en este ejercicio se pide el perimetro de un pentagono regular 
 
-
-public class PentagonoRegular {
-//  nombramos la variable 
-	int  lado;
-	Scanner n;
-	
-	PentagonoRegular(int lado) {
-		
-		
-		getLado();
-		
-	    System.out.println("El perimetro de un pentagono regular es: "+getPerimetro());
-	}
-	// este es para que puedas ingresar el numero de lados se saca la multiplicacion  de todos los lados 
-	
-	int getLado(){
-		n = new Scanner(System.in);
-		System.out.println("Ingresa el valor de los lados");  
-	    lado = n.nextInt(); 
-	    
-		return lado;
-	}
-	
-	void setLado(int lado){
-		this.lado = lado;
-	}
-	
-	int getPerimetro(){
-		return lado*5;
-	}
+public class PentagonoRegular 
+{
+int lado;
+PentagonoRegular(int lado)
+{
+this.lado = lado;
 }
+int getLado()
+{
+return lado;
+}
+void setLado(int lado)
+{
+this.lado = lado;
+}
+int getPerimetro()
+{
+return lado*5;
+}
+}
+
+ 
+
