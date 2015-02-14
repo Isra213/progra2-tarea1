1325d72220e6bdf58998987022848e7a3e00a7ff
Isra213 <israeljosue@unitec.edu>
2015-02-14 16:28:56 -0600

    Update Rectangulo.java

--- src/Rectangulo.java
+++ src/Rectangulo.java
@@ -1,5 +1,7 @@
-import java.util.Scanner;
+// el ejercicio consiste en sacar el area  y para sacar el area se multiplica base por altura 
 
+import java.util.Scanner;
+//nombre mis variables 
 
 public class Rectangulo {
 	int base;
@@ -12,7 +14,7 @@ public class Rectangulo {
 		getAltura();
 	    System.out.println("El area de un rectangulo de base "+base+" y altura "+altura+" es igual a: "+getArea()+" y el perimetro es: "+getPerimetro());
 	}
-	
+	// aqui se pide que ingrese el numero para la base
 	int getBase(){
 		n = new Scanner(System.in);
 		System.out.println("Ingresa la base");  
@@ -24,7 +26,7 @@ public class Rectangulo {
 	void setBase( int base){
 		this.base = base;
 	}
-	
+		// aqui se pide que ingrese el numero para la altura 
 	int getAltura(){
 		n = new Scanner(System.in);
 		System.out.println("Ingresa la altura");  
@@ -35,6 +37,7 @@ public class Rectangulo {
 	void setAltura( int altura){
 		this.altura = altura;
 	}
+	// se efectua la multiplicacion 
 	
 	int getArea(){
 		return base*altura;
